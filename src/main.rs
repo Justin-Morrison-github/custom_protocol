dfed9a301188e601acc2b0be02ffae536dffbc66
Justin Morrison <morrisonxjustin@gmail.com>
2026-05-04 18:59:43 -0400

    Make generated files go in /src/generated

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,11 @@ mod bcp;
 mod codec;
 
 #[allow(dead_code)]
-mod generated_messages {
-    include!(concat!(env!("OUT_DIR"), "/bcp_messages.rs"));
-}
+mod generated;
 
-use generated_messages::MsgB;
+use generated::MsgB;
 
 fn main() {
-    println!("BCP message bindings were generated at build time.");
-
     let a = MsgB {
         count_1: 1,
         count_2: -1,
